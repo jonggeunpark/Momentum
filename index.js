5eb7b81681d6f6d0edfba193f0e6dc57e995d677
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2020-02-05 01:41:07 +0900

    commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const caculator = {
+    plus: function(a,b){
+        return a+b; 
+    },
+    minus: function(a,b){
+        return a-b; 
+    },
+    times: function(a,b){
+        return a*b; 
+    },
+    divided: function(a,b){
+        return a/b; 
+    }
+}
+
+plus = caculator.plus(5,7)
+
+console.log(plus)
